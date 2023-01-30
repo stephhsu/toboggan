c58bdcd3605879f62b9ad79a2a25c4ea2f131551
Nina Zhao <nina.r.zhao@gmail.com>
2023-01-30 12:11:49 -0500

    set db user from .env

--- src/config/storage.config.js
+++ src/config/storage.config.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "me",
+  user: process.wnv.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
